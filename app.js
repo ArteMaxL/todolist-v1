d704d639b49bb27af1e17237d700b07837ed2dc3
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2021-12-30 20:54:23 -0300

    se agrego un ciclo for para iterar sobre la lista de items en el archivo list.ejs

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   var today = new Date();
-  var day = "";
-  var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    day = "Weekend";
-  } else {
-    day = "Weekday";
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  var name = week[today.getDay()];
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {kindOfDay: name});
+  res.render('list', {kindOfDay: day, newListItems: items});
 
+});
+
+app.post("/", function (req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 
