6c6963766dd036e2ce95a327ca4173736d56c91a
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2021-12-31 02:51:40 -0300

    Refactoreo del codigo. Se separo la logica para obtener la fecha en un archivo separado

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -15,16 +16,8 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  let today = new Date();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDate();
   res.render('list', {
     listTitle: day,
     newListItems: items
